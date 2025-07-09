6f9fb366fb7097288522f14815e82c545d51da7a
Abdallah Alnajar <140744532+abdallahalnagar188@users.noreply.github.com>
2025-07-09 20:09:14 +0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:ecommerce_store/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+/// Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
   runApp(const PerfumeStore());
 }
 
